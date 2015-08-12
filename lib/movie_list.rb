e5bad9bffd045361a7a2bafde7d37a95daa1fc4a
nikila-saravanan <nikila.saravanan@gmail.com>
2015-08-12 10:53:24 -0400

    add pstore functionality

--- lib/movie_list.rb
+++ lib/movie_list.rb
@@ -3,8 +3,7 @@ require 'pstore'
 
 class Movie_List
   def initialize
-    @movies = []
-    @movies_store = PStore.new("movies.pstore")
+    @movies_store ||= PStore.new("../movies.pstore")
   end
 
   def run
@@ -53,47 +52,46 @@ class Movie_List
   def add_movie
     puts "What's the movie title?"
     title = gets.chomp
-    movie = Movie.new(title)
-    @movies << movie
     puts "What rating (out of 100) would you like to give this movie?"
     rating = gets.chomp
-    movie.score = rating
-    puts "#{movie.title} has been added to your list with a rating of #{movie.score}."
+    puts "#{title} has been added to your list with a rating of #{rating}."
     @movies_store.transaction do
-      @movies_store[movie.title] = movie.score
+      @movies_store[title] = rating
+      binding.pry
     end
   end
 
   def view_movie
     puts "What movie would you like to see?"
     film = gets.chomp
-    movie = find_movie(film)
-    score = @movies_store.transaction {@movies_store[movie.title]}
-    puts "#{movie.title}"
-    puts "Your Score: #{score}"
-    puts "Rotten Tomatoes Score: #{movie.get_rt_score}"
-    #puts "Metacritic Score: #{movie.get_mc_score}"
-    puts "#{movie.get_rt_consensus}"
-  end
-
-  def find_movie(movie_title)
-    @movies.find do |movie|
-      if movie.title == movie_title
-        return movie
-      end
+    score = @movies_store.transaction { @movies_store[film] }
+    if score != nil
+      movie = Movie.new(film)
+      movie.score = score
+      puts "#{movie.title}"
+      puts "Your Score: #{movie.score}"
+      puts "Rotten Tomatoes Score: #{movie.get_rt_score}"
+      #puts "Metacritic Score: #{movie.get_mc_score}"
+      puts "#{movie.get_rt_consensus}"
+    else
+      puts "That movie is not in your list."
     end
-    puts "That movie is not in your list."
-    run
   end
 
   def change_score
     puts "What film's score would you like to change?"
     film = gets.chomp
-    movie = find_movie(film)
-    puts "What would you rate this movie?"
-    rating = gets.chomp
-    movie.score = rating
-    puts "The rating has been updated, the score for #{movie.title} is now #{movie.score}."
+    score = @movies_store.transaction { @movies_store[film] }
+    if score != nil
+      puts "What would you rate this movie?"
+      rating = gets.chomp
+      @movies_store.transaction do
+        @movies_store[film] = rating
+      end
+      puts "The rating has been updated, the score for #{film} is now #{rating}."
+    else
+      puts "This movie is not in your list."
+    end
   end
 
 
