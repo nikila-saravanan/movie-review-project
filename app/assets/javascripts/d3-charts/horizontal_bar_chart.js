35aaa0634827ae9a6ecdb86d98ff32f37417ea7b
ehawk823 <evan.alexander.hawk@gmail.com>
2015-11-06 01:28:58 -0600

    update

--- app/assets/javascripts/d3-charts/horizontal_bar_chart.js
+++ app/assets/javascripts/d3-charts/horizontal_bar_chart.js
@@ -7,7 +7,3 @@ function bar_chart( element, data ) {
 	    .style("width", function(d) { return x(d) + "px"; })
 	    .text(function(d) { return d; });
 }
-
-$(document).ready(function(){
-	bar_chart( ".horizontal_bar_chart", ["<%= data.join(', ') %>"] );
-});
